34bf6a2462b153bbc1ba4177fc2d258fd60cf963
balkoev <adam22185515@gmail.com>
2019-09-26 14:50:33 +0300

    add input and search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ export default class App extends Component {
       this.createTodoItem('Drink Coffee'),
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch')
-    ]
+    ],
+    term: ''
   };
 
   createTodoItem(label) {
@@ -26,8 +27,8 @@ export default class App extends Component {
       important: false,
       done: false,
       id: this.maxId++
-    }
-  }
+    };
+  };
 
   deleteItem = (id) => {
     this.setState(({ todoData }) => {
@@ -69,7 +70,7 @@ export default class App extends Component {
       newItem,
       ...arr.slice(idx + 1)
     ];
-  }
+  };
 
   onToggleImportant = (id) => {
     this.setState(({ todoData }) => {
@@ -88,9 +89,28 @@ export default class App extends Component {
     });
   };
 
+  search(items, term) {
+    if (term.length === 0) {
+      return items;
+    };
+
+    return items.filter((item) => {
+      return item.label
+            .toLowerCase()
+            .indexOf(term.toLowerCase()) > -1;
+    });
+  };
+
+  onSearchChange = (term) => {
+    this.setState({ term });
+  };
+
   render() {
 
-    const { todoData } = this.state;
+    const { todoData, term } = this.state;
+
+    const visibleItems = this.search(todoData, term);
+
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
 
@@ -98,11 +118,12 @@ export default class App extends Component {
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
+          <SearchPanel
+            onSearchChange={this.onSearchChange} />
           <ItemStatusFilter />
         </div>
         <TodoList
-          todos={this.state.todoData}
+          todos={visibleItems}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone} />
